2d40cf61254d74da09b0bd12e0a6dce10a2056cc
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-30 03:09:07 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,12 +1,3 @@
 require_relative "blackjack.rb"
-def runner
-  welcome
-  card total = initial_round
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
-end
 
- runner
+runner
